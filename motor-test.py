3b550725a8494b083ef01cce632b2a4d8ac791c5
kenjara <kenjara@hotmail.com>
2020-04-18 16:33:36 +0000

    Update motor-test script

--- motor-test.py
+++ motor-test.py
@@ -1,15 +1,16 @@
 import time
 from explorerhat import motor
-from drv8830 import DRV8830, I2C_ADDR1, I2C_ADDR2
+from drv8830 import DRV8830, I2C_ADDR1, I2C_ADDR3
 
 left = DRV8830(I2C_ADDR1)
-right = DRV8830(I2C_ADDR2)
+right = DRV8830(I2C_ADDR3)
 
 # Move forward
+#left.set_direction('forward')
 left.forward()
 right.forward()
-motor.one.speed(1)
-motor.two.speed(1)
+motor.one.forwards()
+motor.two.forwards()
 time.sleep(3.0)
 
 left.brake()
