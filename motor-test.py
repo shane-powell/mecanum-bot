da198b9d82a3158ffc5e3d531f063308151f6c7e
shane-powell <kenjara@hotmail.com>
2020-04-19 13:32:15 +0100

    Working sideways

--- motor-test.py
+++ motor-test.py
@@ -2,40 +2,47 @@ import time
 from explorerhat import motor
 from drv8830 import DRV8830, I2C_ADDR1, I2C_ADDR3
 
+v = 5
+speed = 80
+
 left = DRV8830(I2C_ADDR1)
 right = DRV8830(I2C_ADDR3)
 
+left.set_voltage(0)
+right.set_voltage(0)
+#time.sleep(3)
+
 #Set Voltages
-left.set_voltage(5)
-right.set_voltage(5)
+left.set_voltage(v)
+right.set_voltage(v)
 
 # Move forward
 left.forward()
 right.forward()
-motor.one.forwards()
-motor.two.forwards()
-time.sleep(3.0)
+motor.one.forwards(speed)
+motor.two.forwards(speed)
+time.sleep(0.5)
 
 # Slide to the left
 left.reverse()
 right.forward()
-motor.one.backwards()
-motor.two.forwards()
-time.sleep(3.0)
+motor.one.forwards(speed)
+motor.two.backwards(speed)
+time.sleep(1.5)
 
 # Slide to the right
 left.forward()
 right.reverse()
-motor.one.forwards()
-motor.two.backwards()
-time.sleep(3.0)
+motor.one.backwards(speed)
+motor.two.forwards(speed)
+time.sleep(1.5)
 
 # Take it back now yall
 left.reverse()
 right.reverse()
-motor.one.backwards()
-motor.two.backwards()
-time.sleep(3.0)
+motor.one.backwards(speed)
+motor.two.backwards(speed)
+time.sleep(0.5)
 
 #Clean up
 left.brake()
